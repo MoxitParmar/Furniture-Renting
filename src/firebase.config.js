a3a008cad2c95efcdb42f5da38973611c2e6347b
moxitparmar <moxitparmar2004@gmail.com>
2024-06-29 16:07:39 +0530

    some database changes

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,19 +1,24 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGPZ56KNzQlsRX4XoH8Ax6zN0p0t3FIbA",
-  authDomain: "maltimart-ff741.firebaseapp.com",
-  projectId: "maltimart-ff741",
-  storageBucket: "maltimart-ff741.appspot.com",
-  messagingSenderId: "186880437017",
-  appId: "1:186880437017:web:ca3fc9b4efad8890ac3388",
+  apiKey: "AIzaSyB0aJjXjX7pNyHwytd3S7tj67LMNo-Lhms",
+  authDomain: "furniture-renting-e6415.firebaseapp.com",
+  projectId: "furniture-renting-e6415",
+  storageBucket: "furniture-renting-e6415.appspot.com",
+  messagingSenderId: "944293200972",
+  appId: "1:944293200972:web:d8118ec63fa5efc30ba0e9",
+  measurementId: "G-PDPN3QK7W0"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
 export default app;
