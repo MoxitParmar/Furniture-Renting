71555fd56b3003ecfe675429c0c7c25105e447e7
moxitparmar <moxitparmar2004@gmail.com>
2024-06-29 11:32:52 +0530

    add search in shop

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import CommonSection from "../components/UI/CommonSection";
 import Helmet from "../components/Helmet/Helmet";
 import { Container, Row, Col } from "reactstrap";
 
+import "../styles/shop.css";
+import products from "../assets/data/products";
+import ProductsLists from "../components/UI/ProductsList";
+
 const Shop = () => {
+  const [productsData, setProductsData] = useState(products);
+
+  const handleFilter = (e) => {
+    const filterValue = e.target.value;
+    if (filterValue === "sofa") {
+      const filteredProducts = products.filter(
+        (item) => item.category === "sofa"
+      );
+      setProductsData(filteredProducts);
+    }
+
+    if (filterValue === "mobile") {
+      const filteredProducts = products.filter(
+        (item) => item.category === "mobile"
+      );
+      setProductsData(filteredProducts);
+    }
+    if (filterValue === "chair") {
+      const filteredProducts = products.filter(
+        (item) => item.category === "chair"
+      );
+      setProductsData(filteredProducts);
+    }
+    if (filterValue === "watch") {
+      const filteredProducts = products.filter(
+        (item) => item.category === "watch"
+      );
+      setProductsData(filteredProducts);
+    }
+    if (filterValue === "wireless") {
+      const filteredProducts = products.filter(
+        (item) => item.category === "wireless"
+      );
+      setProductsData(filteredProducts);
+    }
+  };
+
+  const handleSearch = (e) => {
+    const searchTerm = e.target.value;
+
+    const searchedProducts = products.filter((item) =>
+      item.productName.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setProductsData(searchedProducts);
+  };
+
   return (
     <Helmet title="shop">
       <CommonSection title="Products" />
@@ -13,7 +64,7 @@ const Shop = () => {
           <Row>
             <Col lg="3" md="3">
               <div className="filter__widget">
-                <select>
+                <select onChange={handleFilter}>
                   <option>Filter By Category</option>
                   <option value="sofa">Sofa</option>
                   <option value="mobile">Mobile</option>
@@ -34,12 +85,28 @@ const Shop = () => {
             </Col>
             <Col lg="6" md="6">
               <div className="search__box">
-                <input type="text" placeholder="Search....." />
+                <input
+                  type="text"
+                  placeholder="Search....."
+                  onChange={handleSearch}
+                />
                 <span>
                   <i class="ri-search-line"></i>
                 </span>
               </div>
             </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section className="pt-0">
+        <Container>
+          <Row>
+            {productsData.length === 0 ? (
+              <h1 className="text-center fs-4">No products are found!</h1>
+            ) : (
+              <ProductsLists data={productsData} />
+            )}
           </Row>
         </Container>
       </section>
