d3cf290119ddfd8504013749ba970ba2b31d9df0
moxitparmar <moxitparmar2004@gmail.com>
2024-06-29 16:55:28 +0530

    clock timer set

--- src/components/UI/Clock.jsx
+++ src/components/UI/Clock.jsx
@@ -11,7 +11,7 @@ const Clock = () => {
   let interval;
 
   const countDown = () => {
-    const destination = new Date("Apr 22, 2023").getTime();
+    const destination = new Date("july 10, 2024").getTime();
     interval = setInterval(() => {
       const now = new Date().getTime();
       const different = destination - now;
