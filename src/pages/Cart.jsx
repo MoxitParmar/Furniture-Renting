591bac4a32c7e226ab0a73e963c643a167d7bc5f
moxitparmar <moxitparmar2004@gmail.com>
2024-06-29 11:56:19 +0530

    Delete cart and add details

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,77 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import "../styles/cart.css";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/CommonSection";
+import { Container, Row, Col } from "reactstrap";
+
+import { motion } from "framer-motion";
+import { cartActions } from "../redux/slices/cartSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 const Cart = () => {
   const cartItems = useSelector((state) => state.cart.cartItems);
-  console.log(cartItems);
-  return <div>Cart</div>;
+
+  return (
+    <Helmet title="Cart">
+      <CommonSection title="Shopping Cart" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="9">
+              {cartItems.length === 0 ? (
+                <h2 className="fs-4 text-center">No item added to the cart</h2>
+              ) : (
+                <table className="table bordered">
+                  <thead>
+                    <tr>
+                      <th>Image</th>
+                      <th>Title</th>
+                      <th>Price</th>
+                      <th>Qty</th>
+                      <th>Delete</th>
+                    </tr>
+                  </thead>
+
+                  <tbody>
+                    {cartItems.map((item, index) => (
+                      <Tr item={item} key={index} />
+                    ))}
+                  </tbody>
+                </table>
+              )}
+            </Col>
+
+            <Col lg="3"></Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+const Tr = ({ item }) => {
+  const dispatch = useDispatch();
+  const deleteProduct = () => {
+    dispatch(cartActions.deleteItem(item.id));
+  };
+
+  return (
+    <tr>
+      <td>
+        <img src={item.imgUrl} alt="" />
+      </td>
+      <td>{item.productName}</td>
+      <td>${item.price}</td>
+      <td>{item.quantity}px</td>
+      <td>
+        <motion.i
+          whileTap={{ scale: 1.2 }}
+          onClick={deleteProduct}
+          class="ri-delete-bin-line"
+        ></motion.i>
+      </td>
+    </tr>
+  );
 };
 
 export default Cart;
