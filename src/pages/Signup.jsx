326a069af599e76c46e508871d6e08692dcc39d3
moxitparmar <moxitparmar2004@gmail.com>
2024-06-29 12:09:29 +0530

    Signup-choosen file

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -5,8 +5,10 @@ import { Link } from "react-router-dom";
 import "../styles/login.css";
 
 const Signup = () => {
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [file, setFile] = useState(null);
   return (
     <Helmet title="Signup">
       <section>
@@ -16,6 +18,14 @@ const Signup = () => {
               <h3 className="fw-bold mb-4">Signup</h3>
 
               <Form className="auth__form">
+                <FormGroup className="form__group">
+                  <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </FormGroup>
                 <FormGroup className="form__group">
                   <input
                     type="email"
@@ -30,6 +40,12 @@ const Signup = () => {
                     placeholder="Enter your password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input
+                    type="file"
+                    onChange={(e) => setFile(e.target.files[0])}
                   />
                 </FormGroup>
 
