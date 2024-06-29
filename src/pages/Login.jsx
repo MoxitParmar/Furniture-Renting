8d425c93c7b1bcb31f92974fb8c8a34c86c11294
moxitparmar <moxitparmar2004@gmail.com>
2024-06-29 12:05:39 +0530

    signup updates

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,27 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Helmet from "../components/Helmet/Helmet";
 import { Container, Row, Col, Form, FormGroup } from "reactstrap";
 import { Link } from "react-router-dom";
 import "../styles/login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <Helmet title="Login">
       <section>
         <Container>
           <Row>
             <Col lg="6" className="m-auto text-center">
-              <h3 className="fw-bold fs-4">Login</h3>
+              <h3 className="fw-bold mb-4">Login</h3>
 
               <Form className="auth__form">
                 <FormGroup className="form__group">
-                  <input type="email" placeholder="Enter your email" />
+                  <input
+                    type="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </FormGroup>
                 <FormGroup className="form__group">
-                  <input type="password" placeholder="Enter your password" />
+                  <input
+                    type="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
                 </FormGroup>
 
-                <button className="buy__btn auth__btn">Login</button>
+                <button type="submit" className="buy__btn auth__btn">
+                  Login
+                </button>
                 <p>
                   Don't have an account?{" "}
                   <Link to="/signup">Create an account</Link>{" "}
